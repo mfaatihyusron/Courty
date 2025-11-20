51cd5b84a260c551d6b7f92b5521b89ee0098ed9
unknown <muhammadryga936@gmail.com>
2025-11-20 13:00:17 +0700

    db

---
+++ courty.sql
@@ -0,0 +1,181 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Waktu pembuatan: 20 Nov 2025 pada 06.51
+-- Versi server: 10.4.32-MariaDB
+-- Versi PHP: 8.1.25
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `courty`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `booking`
+--
+
+CREATE TABLE `booking` (
+  `id` int(11) NOT NULL,
+  `id_user` int(11) DEFAULT NULL,
+  `id_count` int(11) DEFAULT NULL,
+  `start_time` varchar(50) DEFAULT NULL,
+  `end_time` varchar(50) DEFAULT NULL,
+  `duration_hours` varchar(50) DEFAULT NULL,
+  `total_price` int(11) DEFAULT NULL,
+  `status` varchar(50) DEFAULT NULL,
+  `booking_date` date DEFAULT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `court`
+--
+
+CREATE TABLE `court` (
+  `id_court` int(11) NOT NULL,
+  `id_venue` int(11) DEFAULT NULL,
+  `id_sport` int(11) DEFAULT NULL,
+  `price_per_hour` varchar(50) DEFAULT NULL,
+  `description` text DEFAULT NULL,
+  `profile_photo` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `photo`
+--
+
+CREATE TABLE `photo` (
+  `id_photo` int(11) NOT NULL,
+  `link` varchar(255) DEFAULT NULL,
+  `id_court` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `sport`
+--
+
+CREATE TABLE `sport` (
+  `id_sport` int(11) NOT NULL,
+  `name` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `users`
+--
+
+CREATE TABLE `users` (
+  `id_user` int(11) NOT NULL,
+  `name` varchar(255) DEFAULT NULL,
+  `role` binary(1) DEFAULT NULL,
+  `email` varchar(255) DEFAULT NULL,
+  `telp` varchar(255) DEFAULT NULL,
+  `password` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `venue`
+--
+
+CREATE TABLE `venue` (
+  `id_venue` int(11) NOT NULL,
+  `name` varchar(255) DEFAULT NULL,
+  `address` varchar(255) DEFAULT NULL,
+  `coordinate` varchar(255) DEFAULT NULL,
+  `maps_url` varchar(255) DEFAULT NULL,
+  `description` text DEFAULT NULL,
+  `opening_time` varchar(50) DEFAULT NULL,
+  `closing_time` varchar(50) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indeks untuk tabel `booking`
+--
+ALTER TABLE `booking`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `id_user` (`id_user`);
+
+--
+-- Indeks untuk tabel `court`
+--
+ALTER TABLE `court`
+  ADD PRIMARY KEY (`id_court`),
+  ADD KEY `id_venue` (`id_venue`);
+
+--
+-- Indeks untuk tabel `photo`
+--
+ALTER TABLE `photo`
+  ADD PRIMARY KEY (`id_photo`),
+  ADD KEY `id_court` (`id_court`);
+
+--
+-- Indeks untuk tabel `sport`
+--
+ALTER TABLE `sport`
+  ADD PRIMARY KEY (`id_sport`);
+
+--
+-- Indeks untuk tabel `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id_user`);
+
+--
+-- Indeks untuk tabel `venue`
+--
+ALTER TABLE `venue`
+  ADD PRIMARY KEY (`id_venue`);
+
+--
+-- Ketidakleluasaan untuk tabel pelimpahan (Dumped Tables)
+--
+
+--
+-- Ketidakleluasaan untuk tabel `booking`
+--
+ALTER TABLE `booking`
+  ADD CONSTRAINT `booking_ibfk_1` FOREIGN KEY (`id_user`) REFERENCES `users` (`id_user`);
+
+--
+-- Ketidakleluasaan untuk tabel `court`
+--
+ALTER TABLE `court`
+  ADD CONSTRAINT `court_ibfk_1` FOREIGN KEY (`id_venue`) REFERENCES `venue` (`id_venue`);
+
+--
+-- Ketidakleluasaan untuk tabel `photo`
+--
+ALTER TABLE `photo`
+  ADD CONSTRAINT `photo_ibfk_1` FOREIGN KEY (`id_court`) REFERENCES `court` (`id_court`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
