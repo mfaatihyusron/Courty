0ac660e965f4df0de643d70f66e15e0a0af9f9c3
madiduts <mahdisutejo7@gmail.com>
2025-11-25 23:41:42 +0700

    update app

--- application/controllers/App.php
+++ application/controllers/App.php
@@ -16,9 +16,9 @@ class App extends CI_Controller {
     public function __construct()
     {
         parent::__construct();
-        // Hanya memuat model dan helper yang diperlukan untuk halaman publik
+        // PERBAIKAN: Memuat Model yang diperlukan
         $this->load->model('Model'); 
-        $this->load->model('SportModel'); 
+        $this->load->model('SportModel'); // <--- INI KRITIS!
         $this->load->helper(array('url'));
         $this->load->library(array('session'));
     }
@@ -43,61 +43,21 @@ class App extends CI_Controller {
 
     public function about()
 	{
-        $data['user_name'] = $this->session->userdata('name');
-		$data['content'] = "about"; 
-		$this->load->view('template', $data);
-	}
+	// ... (fungsi lain yang sudah ada)
+    }
 
-    public function view_sport_category($sport_name = 'futsal') // Beri nilai default
+    public function view_sport_category($sport_name = 'futsal')
     {
-        // 1. Ambil data venue berdasarkan nama olahraga (melalui Model)
-        // PERBAIKAN 2: Panggil Model yang benar dan HAPUS SEMUA DUMMY DATA HARDCODED DI SINI
+        // PANGGILAN MODEL YANG MENGGUNAKAN LOGIKA DARI SPORTMODEL.PHP DI ATAS
         $venue_data = $this->SportModel->get_venues_by_sport($sport_name); 
         
-        // 2. Siapkan data untuk dikirim ke View
         $data['user_name'] = $this->session->userdata('name');
         $data['nama_olahraga'] = ucfirst(str_replace('-', ' ', $sport_name)); 
         $data['venue_list'] = $venue_data;
         $data['title'] = 'Reservasi Lapangan ' . $data['nama_olahraga'];
         
-        // 3. Muat View dinamis ke dalam template
         $data['content'] = "sport_category"; 
         $this->load->view('template', $data);
     }
-
-    public function view_venue_detail($id_venue)
-    {
-        // 1. Ambil data Venue
-        $venue_data = $this->Model->get_venue_detail_by_id($id_venue);
-
-        if (empty($venue_data)) {
-            // Handle jika venue tidak ditemukan
-            show_404();
-            return;
-        }
-
-        // 2. Ambil daftar Lapangan (Courts) yang dimiliki venue tersebut
-        $court_list = $this->Model->get_courts_by_venue_id($id_venue);
-
-        // 3. Siapkan data untuk View
-        $data['user_name'] = $this->session->userdata('name');
-        $data['venue'] = $venue_data;
-        $data['courts'] = $court_list;
-
-        // Data dummy untuk Gallery Photo (Untuk simulasi, asumsikan ada 3 foto tambahan selain foto profil)
-        $data['gallery_photos'] = [
-            ['url' => 'https://placehold.co/800x600/926699/FFFFFF?text=GALERI+FOTO+1'],
-            ['url' => 'https://placehold.co/800x600/B9CF32/FFFFFF?text=GALERI+FOTO+2'],
-            ['url' => 'https://placehold.co/800x600/808080/FFFFFF?text=GALERI+FOTO+3'],
-        ];
-
-        $data['content'] = 'venue_detail'; // Nama file View baru
-        $this->load->view('template', $data);
-    }
-    
-    public function formvalidasi()
-	{
-		$data['content'] = "formvalidasi"; 
-		$this->load->view('template', $data);
-	}
+    // ... (sisa fungsi lain)
 }
